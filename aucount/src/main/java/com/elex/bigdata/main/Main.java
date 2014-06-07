2670b1a1e331e897bc016995d26f2744c210dd09
griffinl <lq0987@126.com>
2014-06-08 00:03:12 +0800

    update

--- aucount/src/main/java/com/elex/bigdata/main/Main.java
+++ aucount/src/main/java/com/elex/bigdata/main/Main.java
@@ -17,6 +17,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
+        System.out.println(args.length + " - " + args[0]);
         if(args.length != 2){
             System.err.println("Usage : filename");
             System.exit(-1);
