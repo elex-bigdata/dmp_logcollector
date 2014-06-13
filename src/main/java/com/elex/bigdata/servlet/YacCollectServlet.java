294e6b813ff8452df9b0b39bf76d8219b65b4265
griffinl <lq0987@126.com>
2014-06-13 10:52:11 +0800

    update

--- src/main/java/com/elex/bigdata/servlet/YacCollectServlet.java
+++ src/main/java/com/elex/bigdata/servlet/YacCollectServlet.java
@@ -1,5 +1,6 @@
 package com.elex.bigdata.servlet;
 
+import com.elex.bigdata.util.DMUtils;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
@@ -34,90 +35,68 @@ public class YacCollectServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        req.setCharacterEncoding("utf-8");  //设置编码
+        String decode = req.getParameter("ts");
+        if(decode == null || decode.length() < 4){
+            resp.getWriter().write("{'status':'fail','msg':'param ts is invalid'}");
+            return;
+        }
 
-        //获得磁盘文件条目工厂
-        DiskFileItemFactory factory = new DiskFileItemFactory();
-        //获取文件需要上传到的路径
+        req.setCharacterEncoding("utf-8");  //设置编码
 
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
         String day = sdf.format(new Date());
-
         String path = prefixPath + day;
-        String decode = req.getParameter("ts");
-        String ip = req.getHeader("X-Forwarded-For") != null ? req.getHeader("X-Forwarded-For") : req.getRemoteAddr();
-        LOG.debug("IP : " + ip);
-        LOG.debug("Decode : " + decode);
 
+        String ip = req.getHeader("X-Forwarded-For") != null ? req.getHeader("X-Forwarded-For") : req.getRemoteAddr();
+        //TODO；存IP
         File file = new File(path);
         if( !file.exists() || !file.isDirectory()){
             file.mkdir();
         }
 
-        LOG.debug("Path : " + path);
-
-        //如果没以下两行设置的话，上传大的 文件 会占用 很多内存，
-        //设置暂时存放的 存储室 , 这个存储室，可以和 最终存储文件 的目录不同
-        /**
-         * 原理 它是先存到 暂时存储室，然后在真正写到 对应目录的硬盘上，
-         * 按理来说 当上传一个文件时，其实是上传了两份，第一个是以 .tem 格式的
-         * 然后再将其真正写到 对应目录的硬盘上
-         */
+        DiskFileItemFactory factory = new DiskFileItemFactory();
         factory.setRepository(new File(path));
-        //设置 缓存的大小，当上传文件的容量超过该缓存时，直接放到 暂时存储室
         factory.setSizeThreshold(1024*1024) ;
 
-        //高水平的API文件上传处理
         ServletFileUpload upload = new ServletFileUpload(factory);
-
         try {
             //可以上传多个文件
             List<FileItem> list = (List<FileItem>)upload.parseRequest(req);
-
             for(FileItem item : list){
-                //获取表单的属性名字
-                String name = item.getFieldName();
 
-                //如果获取的 表单信息是普通的 文本 信息
-                if(item.isFormField()){
-                    //获取用户具体输入的字符串 ，名字起得挺好，因为表单提交过来的是 字符串类型的
-                    String value = item.getString() ;
+                if(!item.isFormField()){
 
-                    req.setAttribute(name, value);
-                }
-                //对传入的非 简单的字符串进行处理 ，比如说二进制的 图片，电影这些
-                else
-                {
-                    /**
-                     * 以下三步，主要获取 上传文件的名字
-                     */
-                    //获取路径名
                     String value = item.getName() ;
-                    //索引到最后一个反斜杠
-                    int start = value.lastIndexOf("\\");
-                    //截取 上传文件的 字符串名字，加1是 去掉反斜杠，
-                    String filename = value.substring(start+1);
+                    String filename = value.substring(value.lastIndexOf("\\")+1);
 
-                    LOG.debug("filename : " + filename);
-                    //真正写到磁盘上
-                    //它抛出的异常 用exception 捕捉
+                    String sed = decode.substring(decode.length() - 4);
+                    LOG.debug("filename : " + filename + "， IP : " + ip + "， Seed : " + sed + ", size :" + item.getSize());
 
-                    item.write( new File(path,filename) );//第三方提供的
+                    OutputStream out = new FileOutputStream(new File(path,sed + "_" + filename));
+                    InputStream in = item.getInputStream() ;
 
+                    int length = 0 ;
+                    byte [] buf = new byte[1024] ;
 
+                    byte[] seed =sed.getBytes();
+                    int currentPosition = 0;
+                    while( (length = in.read(buf) ) != -1){
+                        //解密
+                        DMUtils.decode(buf,seed,currentPosition);
+                        currentPosition += length;
+
+                        out.write(buf, 0, length);
+                    }
+
+                    in.close();
+                    out.close();
                 }
             }
-
-
-
-        } catch (FileUploadException e) {
-            e.printStackTrace();
+            resp.getWriter().write("{'status':'success','msg':''}");
         }catch (Exception e) {
-
-            e.printStackTrace();
+            LOG.error(e.getMessage());
+            resp.getWriter().write("{'status':'fail','msg':'"+e.getMessage()+"'}");
         }
-
-        resp.getWriter().write("ok");
 
     }
 }
---
+++ src/main/java/com/elex/bigdata/util/DMUtils.java
@@ -0,0 +1,31 @@
+package com.elex.bigdata.util;
+
+import java.io.IOException;
+
+/**
+ * Author: liqiang
+ * Date: 14-6-13
+ * Time: 上午9:19
+ */
+public class DMUtils {
+
+    public static void decode(byte[] b,byte[] seed,int currentPosition) throws IOException {
+        int len = b.length;
+        int step = 4;
+
+        for(int i=0;i<len - step + 1  ; i += step){
+            for(int j=0;j<step;j++){
+                b[i + j] = (byte)(b[i + j] ^ seed[j]);
+            }
+
+            byte[] tseed = intToByteArray((currentPosition + i)/step);
+            for(int k=0;k<4;k++){
+                seed[k] ^= tseed[k];
+            }
+        }
+    }
+
+    public static byte[] intToByteArray(int a){
+        return new byte[] { (byte) ((a >> 24) & 0xFF), (byte) ((a >> 16) & 0xFF), (byte) ((a >> 8) & 0xFF), (byte) (a & 0xFF) };
+    }
+}
