3337992dce80b01ca055e6c29ab9e9d8e2af3a2d
griffinl <lq0987@126.com>
2014-06-16 09:36:59 +0800

    update

--- bin/check_yac_zip.sh
+++ bin/check_yac_zip.sh
@@ -5,7 +5,7 @@ logdir=/data/upload/$(date -d"-5 mins" +"%Y%m%d")/
 limit=20971520 #暂定每天日志上限20G左右
 size=`du -s /data/upload/ | awk '{print $1}'`
 
-for f in `find /data/upload/20140613/ -name *.zip`
+for f in `find ${logdir} -name *.zip`
 do
   if [ ${size} -gt ${limit}];then
     echo "Remove file ${f} as over ${limit} k"
