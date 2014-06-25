af7f4e4ec486f76800e560a6c6e7e9a4683dda9d
griffinl <lq0987@126.com>
2014-06-25 11:42:46 +0800

    update

--- bin/check_yac_zip.sh
+++ bin/check_yac_zip.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 logdir=/data/log/yac/$(date -d"-5 mins" +"%Y%m%d")/
-hour=$(`date -d"-5 mins" +"%H"`)
+hour=$(date -d"-5 mins" +"%H")
 
 limit=4194304 #暂定每小时日志上限40G左右
 size=`du -s ${logdir}*_${hour}.dat | awk '{sum+=$1;}END{print sum}'`
