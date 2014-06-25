6b3f1fcd70279c8d5b225f8ba6dc9acd1873229c
griffinl <lq0987@126.com>
2014-06-25 11:38:01 +0800

    update

--- bin/check_yac_zip.sh
+++ bin/check_yac_zip.sh
@@ -4,11 +4,11 @@ logdir=/data/log/yac/$(date -d"-5 mins" +"%Y%m%d")/
 hour=$(`date -d"-5 mins" +"%H"`)
 
 limit=4194304 #暂定每小时日志上限40G左右
-size=`du -s ${logdir}/*_${hour}.dat | awk '{sum+=$1;}END{print sum}'`
+size=`du -s ${logdir}*_${hour}.dat | awk '{sum+=$1;}END{print sum}'`
 
 for f in `find ${logdir} -name *.zip`
 do
-  if [ ${size} -gt ${limit}];then
+  if [ ${size} -gt ${limit} ];then
     echo "Remove file ${f} as over ${limit} k"
   else
     echo "Unzip and remove file ${f}"
