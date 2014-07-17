9316985988089a84734dfa01cd1664e8ca809b20
griffinl <lq0987@126.com>
2014-07-17 11:48:51 +0800

    update

--- src/main/java/com/elex/bigdata/util/YACConstants.java
+++ src/main/java/com/elex/bigdata/util/YACConstants.java
@@ -20,7 +20,7 @@ public class YACConstants {
 
     public static final String log_dir_path = "/data/log/yac/";
 
-    public static final String url_filter_keywords = "callback=|\\.xml|=http|google-analytics|googlebanner|ad\\.|ads\\.|ad2\\.|\\.ad|192\\.168\\.|\\.ytimg\\.com|ajax|api|image|favicon|\\.doubleclick|s\\.youtube\\.com|&async=|=async&|livetr\\.gostream\\.nl|---|___|/cdn/cdn\\.";
+    public static final String url_filter_keywords = "callback=|\\.xml|=http|google-analytics|googlebanner|ad\\.|ads\\.|ad2\\.|\\.ad|192\\.168\\.|\\.ytimg\\.com|ajax|api|image|favicon|\\.doubleclick|s\\.youtube\\.com|&async=|=async&|livetr\\.gostream\\.nl|---|___|/cdn/cdn\\.|adserver";
     public static final String url_filter_suffix = "gif|GIF|jpg|JPG|png|PNG|ico|ICO|css|flv|CSS|sit|SIT|eps|EPS|wmf|WMF|zip|ZIP|ppt|PPT|mpg|MPG|xls|XLS|gz|GZ|rpm|RPM|tgz|TGZ|mov|MOV|exe|EXE|jpeg|JPEG|bmp|BMP|js|JS|jxr|f3d|woff|svg|AMF|amf|ttf|swf|txt";
 
     public static String URL_FILTER_REG = ".*(("+url_filter_keywords+")|(/"+url_filter_suffix+")|(\\.("+url_filter_suffix+"))).*";
